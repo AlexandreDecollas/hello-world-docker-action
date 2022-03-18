c0a855a57541b6254982ba552782ff7edd3cc8f8
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2022-03-18 19:47:40 +0100

    eventstore

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+docker run -d --name eventstore-eventbus-E2E -it -p 2113:2113 -p 1113:1113 eventstore/eventstore:release-5.0.8
